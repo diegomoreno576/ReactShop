c733b58b2b765d2f512a3bd3abfdebfd0e627d1d
diegomoreno576 <diegomoreno576@gmail.com>
2021-10-25 18:23:32 -0400

    homepage github

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -19,7 +19,7 @@ const App = () => {
 	const initialState = useInitialState();
 	return (
 		<AppContext.Provider value={initialState}>
-			<BrowserRouter>
+			<BrowserRouter "/ReactShop/">
 				<Layout>
 					<Switch>
 						<Route exact path="/" component={Home} />
